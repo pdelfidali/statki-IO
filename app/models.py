e72eaf0aaf99cfbabe70605d0cd3db3645ffb418
pdelfidali <piotrusdf@gmail.com>
2021-11-16 11:32:43 +0100

    Login Register commit

---
+++ app/models.py
@@ -0,0 +1,28 @@
+from . import db, login_manager
+from werkzeug.security import  generate_password_hash, check_password_hash
+from flask_login import UserMixin
+
+
+class User(db.Model, UserMixin):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(25), unique=True, index=True)
+    password_hash = db.Column(db.String(128))
+
+    @property
+    def password(self):
+        raise AttributeError('User.password is protected')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
+
+    def __repr__(self):
+        return f'{self.id}: {self.username}'

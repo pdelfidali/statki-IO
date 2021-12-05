a73f1aefbee1f2db299b3db86b92fa042843cc4c
pdelfidali <piotrusdf@gmail.com>
2021-12-05 20:32:43 +0100

    Player vs Player pregame - 2nd player login, nicks in game change correctly

--- app/game/views.py
+++ app/game/views.py
@@ -1,10 +1,14 @@
-from flask import request, jsonify, redirect, render_template, json
+from flask import request, jsonify, redirect, render_template, json, flash, session
 from sqlalchemy import true, false
 
 from . import game
 from ..statistics.views import upload_resuls_after_game
 from .. import db
-from ..models import Game
+from ..models import Game, User
+from flask_login import current_user, login_required
+from wtforms import SubmitField, StringField, PasswordField
+from wtforms.validators import DataRequired
+from flask_wtf import FlaskForm
 
 
 @game.route('/game_summary', methods=['GET', 'POST'])
@@ -21,8 +25,9 @@ def summary():
     return redirect('/game')
 
 
+@login_required
 @game.route('/game', methods=['POST', 'GET'])
-def game():
+def play():
     return render_template('game.html')
 
 
@@ -37,3 +42,39 @@ def add_game(gameData):
     )
     db.session.add(game)
     db.session.commit()
+
+
+@game.route('/pregame', methods=['POST', 'GET'])
+def pregame():
+    return render_template('pregame.html')
+
+
+class LogoutForm(FlaskForm):
+    logout = SubmitField('Wyloguj się')
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Wprowadź swój nick:', validators=[DataRequired(message='Pole nie może być puste')])
+    password = PasswordField('Wprowadź hasło:', validators=[DataRequired(message='Pole nie może być puste')])
+    submit = SubmitField('Zaloguj się')
+
+
+@login_required
+@game.route('/game/pvp', methods=['POST', 'GET'])
+def pvp():
+    form = LoginForm()
+    form2 = LogoutForm()
+    player2 = None
+    if form2.validate_on_submit() and form2.logout.data:
+        session.pop('Second Player', None)
+        print(form2.logout.data)
+    elif session.get('Second Player'):
+        player2 = User.query.filter_by(username=session.get('Second Player')).first()
+    elif form.validate_on_submit():
+        player2 = User.query.filter_by(username=form.username.data).first()
+        if player2 and player2.verify_password(form.password.data) and player2.username != current_user.username:
+            session['Second Player'] = player2.username
+            flash(f'Poprawnie zalogowano użytkownika o nicku {player2.username}!')
+        else:
+            player2 = None
+    return render_template('pvp.html', form=form, form2=form2, player2=player2)
